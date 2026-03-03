7f3772b73eb20ed323910f190e2105a0d0c9d441
Kornauhov <kornauhovsergej@gmail.com>
2026-03-03 18:38:45 +0100

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,12 @@
-// NOTE: Diese Konfiguration stammt 1:1 aus deiner bisherigen single-file Version.
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyBGqPXXcmJTbL3PVrn-PKL0Gig45r6GPbQ",
   authDomain: "steg-lager-test.firebaseapp.com",
   databaseURL: "https://steg-lager-test-default-rtdb.europe-west1.firebasedatabase.app",
@@ -9,3 +16,7 @@ export const firebaseConfig = {
   appId: "1:655203951825:web:9edcfb40f29ed70f61d1f0",
   measurementId: "G-Q6FX41P556"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
